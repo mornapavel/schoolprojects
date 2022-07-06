5b7fbc42535ad0c449046c6c9191bba6f8ed717c
Morna Pavel <98264139+mornapavel@users.noreply.github.com>
2022-07-06 17:56:53 +0300

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, x;
+            Console.WriteLine("Introduceti a = ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti b = ");
+            b = float.Parse(Console.ReadLine());
+            if (a == 0)
+                if (b == 0)
+                    Console.WriteLine("ecuatia nederteminata ");
+                else Console.WriteLine("Ecuatia imposibila ");
+            else
+            {
+                x = -b / a;
+                Console.WriteLine("Solutia este x = {0}", x );
+            }
+            Console.ReadKey();
+        }
+    }
+}
